bf2984e99f45ee5b1e786cac345c0b9537e92907
пуки <anastasiaptasenko01@gmail.com>
2025-10-27 00:20:39 +0200

    feat: add formatNumber with NumberFormatOptions

--- src/demo.ts
+++ src/demo.ts
@@ -2,3 +2,5 @@ import { add, capitalize } from './index';
 
 console.log('sum(typed):', add(2, 3));
 console.log('capitalize(typed):', capitalize('hello'));
+
+console.log('format(ok):', formatNumber(123.456, { precision: 2 }));
